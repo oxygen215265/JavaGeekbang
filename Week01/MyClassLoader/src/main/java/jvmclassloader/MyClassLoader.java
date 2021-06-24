b85c15979ca286bba1a6efc4c187e41b3c260a18
HeTianxiang <945215265@qq.com>
2021-06-24 15:47:23 +0800

    add classloader

---
+++ Week01/MyClassLoader/src/main/java/jvmclassloader/MyClassLoader.java
@@ -0,0 +1,49 @@
+package jvmclassloader;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyClassLoader extends ClassLoader {
+
+
+    public static void main(String[] args)  {
+
+        try {
+            MyClassLoader classLoader = new MyClassLoader();
+
+            Class<?> hellpClass = classLoader.loadClass("Hello");
+            Method method = hellpClass.getMethod("hello");
+            method.invoke(hellpClass.newInstance());
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get("G:\\javaWork\\Hello.xlass"));
+            for (int i = 0; i < bytes.length; i++) {
+                byte temp = bytes[i];
+                bytes[i] = (byte) (255-temp);
+            }
+
+            //verify result
+            Files.write(Paths.get("G:\\javaWork\\Hello.class"), bytes, StandardOpenOption.CREATE);
+            return defineClass(name,bytes,0,bytes.length);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+}
